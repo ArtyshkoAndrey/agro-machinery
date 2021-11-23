c09025de89bbcad91d703bf28b58ac7bfb9abcf4
Fulliton <artyshko.andrey@gmail.com>
2021-11-23 13:32:08 +0700

    Seeder Category

---
+++ database/seeders/CategorySeed.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Image;
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeed extends Seeder
+{
+  public array $data = [
+   'ru' => [
+     'name' => 'Посев и посадка',
+     'description' => 'Посев и посадка'
+   ],
+    'en' => [
+      'name' => 'Sowing and planting',
+      'description' => 'Sowing and planting'
+    ],
+    'to_index' => true
+  ];
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run(): void
+  {
+    $dir = 'storage/' . date('Y/m/d') . '/';
+
+    if(Category::all()->count() === 0) {
+      $dir .= 'track_1.png';
+      \File::copy(public_path('images/user/track_1.png') ,  public_path($dir));
+
+      $category = new Category($this->data);
+      $category->save();
+
+      $image = new Image();
+      $image->name = 'track_1.png';
+      $image->path = $dir;
+      $image->order = 1;
+      $image->size =\File::size(public_path($dir));
+      $image->save();
+
+      $category->images()->saveMany([$image]);
+      $category->save();
+    }
+  }
+}
